4abcacea41a15948902fe021d417271f68480ab8
unknown <arifulin95r@gmail.com>
2022-05-25 11:17:37 +0200

    début de dynamisation home page et rectification page about

--- wp-content/themes/pokefolio/parts/home/about-me.php
+++ wp-content/themes/pokefolio/parts/home/about-me.php
@@ -3,12 +3,12 @@
     <div class="wrapper">
         <div class="about">
             <div class="about__image">
-                <img data-aos="fade-up" data-aos-duration="3000"  alt="about" src="<?php bloginfo('template_directory') ?>/img/about-me.jpg">
+                <img data-aos="fade-up" data-aos-duration="3000"  alt="about" src="<?php echo CFS()->get( 'about_image', CFS()->options->page( 'Options' ) ); ?>">
             </div>
             <div class="about__text">
                 <h3>About Me</h3>
-                <h1>My Name is Arnaud.</h1>
-                <?php the_content() ?>
+                <h1><?php echo CFS()->get( 'about_introduction', CFS()->options->page( 'Options' ) ); ?></h1>
+                <?php echo CFS()->get( 'about_presentation', CFS()->options->page( 'Options' ) ); ?>
                 <button data-aos="fade-up" data-aos-duration="2600" class="button button--pink"><a href="<?php bloginfo('template_directory') ?>/about">Learn
                         More</a></button>
             </div>
